932b08d027a674c2a5f67e7817b9248186853bc0
Xavi <xaviibarba@gmail.com>
2026-06-04 16:35:51 -0300

    feat: add authenticated user endpoint

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -2,6 +2,8 @@ using Syrla.Application.Common;
 using Microsoft.AspNetCore.Mvc;
 using Syrla.Application.DTOs;
 using Syrla.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace Syrla.API.Controllers;
 
@@ -12,9 +14,9 @@ public class AuthController : ControllerBase
     private readonly IAuthService _service;
 
     public AuthController(IAuthService service)
-{
-    _service = service;
-}
+    {
+        _service = service;
+    }
 
     [HttpPost("login")]
     public async Task<IActionResult> Login(LoginDto dto)
@@ -22,17 +24,40 @@ public class AuthController : ControllerBase
         var token = await _service.LoginAsync(dto);
 
         if (token is null)
-{
-    return Unauthorized(
-        ApiResponse<string>.Fail("Credenciais inválidas.")
-    );
-}
+        {
+            return Unauthorized(
+                ApiResponse<string>.Fail("Credenciais inválidas.")
+            );
+        }
 
-       return Ok(
-    ApiResponse<object>.Ok(
-        new { token },
-        "Login realizado com sucesso."
-    )
-);
+        return Ok(
+            ApiResponse<object>.Ok(
+                new { token },
+                "Login realizado com sucesso."
+            )
+        );
+    }
+
+    [Authorize]
+    [HttpGet("me")]
+    public IActionResult Me()
+    {
+        var id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        var name = User.FindFirst(ClaimTypes.Name)?.Value;
+
+        var email = User.FindFirst(ClaimTypes.Email)?.Value;
+
+        return Ok(
+            ApiResponse<object>.Ok(
+                new
+                {
+                    Id = id,
+                    Name = name,
+                    Email = email
+                },
+                "Usuário autenticado."
+            )
+        );
     }
 }
